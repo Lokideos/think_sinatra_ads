36d76c6f16bf646aebfdea0d5ca8211cb48df3c6
lokideos <archmage09@gmail.com>
2020-07-21 15:01:28 +0300

    Get user_id from auth service during ads creation

--- app/helpers/api_errors.rb
+++ app/helpers/api_errors.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'sinatra/extension'
 
 module ApiErrors
@@ -6,10 +8,10 @@ module ApiErrors
   helpers do
     def error_response(error_messages)
       errors = case error_messages
-      when Sequel::Model
-        ErrorSerializer.from_model(error_messages)
-      else
-        ErrorSerializer.from_messages(error_messages)
+               when Sequel::Model
+                 ErrorSerializer.from_model(error_messages)
+               else
+                 ErrorSerializer.from_messages(error_messages)
       end
 
       json errors
@@ -29,5 +31,10 @@ module ApiErrors
   error Validations::InvalidParams, KeyError do
     status 422
     error_response I18n.t(:missing_parameters, scope: 'api.errors')
+  end
+
+  error Auth::Unauthorized do
+    status 403
+    error_response I18n.t(:unauthorized, scope: 'api.errors')
   end
 end
--- config/application_loader.rb
+++ config/application_loader.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module ApplicationLoader
   extend self
 
@@ -19,6 +21,7 @@ module ApplicationLoader
   end
 
   def require_app
+    require_file 'app/helpers/auth'
     require_dir 'app/helpers'
     require_file 'config/application'
     require_file 'app/services/basic_service'
@@ -36,7 +39,7 @@ module ApplicationLoader
 
   def require_dir(path)
     path = File.join(root, path)
-    Dir["#{path}/**/*.rb"].each { |file| require file }
+    Dir["#{path}/**/*.rb"].sort.each { |file| require file }
   end
 
   def root
