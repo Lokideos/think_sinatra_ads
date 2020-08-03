1237d03e4e73cd7f112111d201077c8201a2a54f
lokideos <archmage09@gmail.com>
2020-08-03 19:44:51 +0300

    Add logs to ads service

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
+require 'sinatra/custom_logger'
+
 class Application < Sinatra::Base
+  helpers Sinatra::CustomLogger
   helpers Validations
 
   configure do
---
+++ config/initializers/logger.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+Application.configure do |app|
+  logger = Ougai::Logger.new(
+    app.root.concat('/', Settings.logger.path),
+    level: Settings.logger.level
+  )
+
+  logger.before_log = lambda do |data|
+    data[:service] = { name: Settings.app.name }
+    data[:request_id] ||= Thread.current[:request_id]
+  end
+
+  app.set :logger, logger
+end
+
+Sequel::Model.db.loggers.push(Application.logger)
