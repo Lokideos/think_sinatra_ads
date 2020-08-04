b29218be9852a94946d038277e5919679b60c039
lokideos <archmage09@gmail.com>
2020-08-04 14:48:24 +0300

    Add logs for debug (commented), log updating coorinates for ads; add request_id upon authentication via RPC

--- config/initializers/consumer.rb
+++ config/initializers/consumer.rb
@@ -6,8 +6,15 @@ queue = channel.queue('ads', durable: true)
 
 queue.subscribe do |_delivery_info, properties, payload|
   payload = JSON(payload)
+  Thread.current[:request_id] = properties.headers['request_id']
   lat, lon = payload['coordinates']
   Ads::UpdateService.call(payload['id'], lat: lat, lon: lon)
+
+  Application.logger.info(
+    'update_ads_coordinates',
+    ad: payload['id'],
+    coordinates: payload['coordinates']
+  )
 
   exchange.publish(
     '',
--- config/initializers/logger.rb
+++ config/initializers/logger.rb
@@ -6,6 +6,14 @@ Application.configure do |app|
     level: Settings.logger.level
   )
 
+  # For debug
+  # logger = Ougai::Logger.new(
+  #   STDOUT,
+  #   level: Settings.logger.level
+  # )
+  #
+  # logger.formatter = Ougai::Formatters::Readable.new
+
   logger.before_log = lambda do |data|
     data[:service] = { name: Settings.app.name }
     data[:request_id] ||= Thread.current[:request_id]
